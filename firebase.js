6c8e48ae348443ee3dc209729240d8e79dd042f9
amol1436 <amolwashimkar8888@gmail.com>
2025-06-16 22:08:21 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDGAjXCIdqSx_FhChoLUjAQ0kN5HSQbTF0",
   authDomain: "amvibe-28f17.firebaseapp.com",
   projectId: "amvibe-28f17",
-  storageBucket: "amvibe-28f17.firebaseapp.com",
+  storageBucket: "amvibe-28f17.firebasestorage.app",
   messagingSenderId: "452233184351",
   appId: "1:452233184351:web:4781d4f09e66a8643c07ea",
   measurementId: "G-SGP0CJFDGE"
@@ -14,4 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export { auth };
+const db = getFirestore(app);
+
+export { auth, db };
