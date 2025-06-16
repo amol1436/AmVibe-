3045be5c12a8c8c4f9a5b12e36652532466fbe8f
amol1436 <amolwashimkar8888@gmail.com>
2025-06-16 09:20:39 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGAjXCIdqSx_FhChoLUjAQ0kN5HSQbTF0",
+  authDomain: "amvibe-28f17.firebaseapp.com",
+  projectId: "amvibe-28f17",
+  storageBucket: "amvibe-28f17.firebaseapp.com",
+  messagingSenderId: "452233184351",
+  appId: "1:452233184351:web:4781d4f09e66a8643c07ea",
+  measurementId: "G-SGP0CJFDGE"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth };
